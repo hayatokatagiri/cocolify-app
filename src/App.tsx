b56fc60140dfc280e34de71037afbc00194c7bbb
hayatokatagiri <zurqnd@gmail.com>
2023-06-08 22:29:36 +0900

    App.tsxのボタンを2度押すと表示が消えるように修正する前の状態をコミット

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+//MoneyButton.tsx、OtherButton.tsxはあとで削除する
+//（機能をApp.tsxに移植済み）
 import React, { useState } from "react";
 import MentalButton from "./components/mental/MentalButton";
 // import MoneyButton from "./components/money/MoneyButton";
 import MoneyResult from "./components/money/MoneyResult";
-import OtherButton from "./components/other/OtherButton";
+//import OtherButton from "./components/other/OtherButton";
 import PhysicalButton from "./components/physical/PhysicalButton";
 import { Box, Button, Typography } from "@mui/material";
 import "./cocolify.css";
@@ -13,9 +15,21 @@ import SNSButton from "./components/SNSButton";
 
 const App = () => {
   const [showLocation, setShowLocation] = useState(false);
+  const [showTextBox, setShowTextBox] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-  const handleButtonClick = () => {
+  const handleMoneyButtonClick = () => {
     setShowLocation(true);
+  };
+
+  const handleOtherButtonClick = (): void => {
+    setShowTextBox(true);
+  };
+
+  const handleOtherChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    setInputValue(e.target.value);
   };
   return (
     <Box
@@ -33,17 +47,37 @@ const App = () => {
       </header>
       <Typography variant="h4">お困りのことはなんですか？</Typography>
       <Box display="flex" flexDirection="row" gap={1}>
-        <Button variant="contained" color="success" onClick={handleButtonClick}>
+        <Button
+          variant="contained"
+          color="success"
+          onClick={handleMoneyButtonClick}
+        >
           お金
         </Button>
         <MentalButton />
         <PhysicalButton />
-        <OtherButton />
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleOtherButtonClick}
+        >
+          その他
+        </Button>
       </Box>
       <Box>
         <VoiceButton />
       </Box>
       <Box>{showLocation && <MoneyResult />}</Box>
+      {showTextBox && (
+        <p>
+          <textarea
+            value={inputValue}
+            onChange={handleOtherChange}
+            placeholder="お悩みの内容を入力してください"
+            rows={5} // 表示する行数を指定
+          />
+        </p>
+      )}
       <Box>
         <SNSButton />
       </Box>
